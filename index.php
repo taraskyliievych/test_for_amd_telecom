5371640b656850d56bfaeafe335712d4f72a8314
taras_kyliievych <taras.kuliievych@agiliway.com>
2020-07-03 02:32:47 +0300

    Create index.php with implementation of functionality

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-type: application/json');
+
+require 'Weather.php';
+require 'Routee.php';
+
+$temp = Weather::getTemperature('Thessaloniki');
+
+if (empty($temp)) {
+    return;
+}
+
+if ($temp > 20) {
+    $messageText = 'Your name and Temperature more than 20C. ' . $temp . 'C';
+} else {
+    $messageText = 'Your name and Temperature less than 20C. ' . $temp . 'C';
+}
+
+$phoneNumber = '+30  6948872100';
+$routeeResponse = Routee::sendSMS('57cd83a3e4b0464b9119ba46', 'OXr7WYcP9A', $phoneNumber, $messageText);
+
+echo json_encode(['result_text' => $messageText, 'routee_response' => $routeeResponse]);
